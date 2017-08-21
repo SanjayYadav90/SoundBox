e48521c50d73e176a48a35e3648fb321e01b6173
Sanjay Yadav <sanjaykumar5c9@yahoo.com>
2017-08-21 22:54:30 +0530

    Page of frontend of the application.

---
+++ blog.php
@@ -0,0 +1,294 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Mosaic a Entertainment Category Flat Bootstrap Responsive Website Template | Blogs :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mosaic Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.css" type='text/css' />
+<!-- //lined-icons -->
+ <!-- Meters graphs -->
+<script src="js/jquery-2.1.4.js"></script>
+
+</head> 
+   <!-- /w3layouts -->
+ <body class="sticky-header left-side-collapsed"  onload="initMap()">
+    <section>
+      <!-- left side start-->
+ <?php include("common/side-menu.php"); ?>
+		<!-- left side end-->
+		 <!-- /agileinfo -->
+					<!-- app-->
+			<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog facebook" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body">
+							<div class="app-grids">
+								<div class="app">
+						<div class="col-md-5 app-left mpl">
+							<h3>Mosaic mobile app on your smartphone!</h3>
+							<p>Download and Avail Special Songs Videos and Audios.</p>
+							<div class="app-devices">
+								<h5>Gets the app from</h5>
+								<a href="#"><img src="images/1.png" alt=""></a>
+								<a href="#"><img src="images/2.png" alt=""></a>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-7 app-image">
+							<img src="images/apps.png" alt="">
+						</div>
+						<div class="clearfix"></div>
+					</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //app-->
+			 <!-- /agile-its -->
+		<!-- signup -->
+			<div class="modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+							<div class="sign-grids">
+								<div class="sign">
+									<div class="sign-left">
+										<ul>
+											<li><a class="fb" href="#"><i></i>Sign in with Facebook</a></li>
+											<li><a class="goog" href="#"><i></i>Sign in with Google</a></li>
+											<li><a class="linkin" href="#"><i></i>Sign in with Linkedin</a></li>
+										</ul>
+									</div>
+									<div class="sign-right">
+										<form action="#" method="post">
+											<h3>Create your account </h3>
+											<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+											<input type="text" value="Mobile number" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Mobile number';}" required="">
+											<input type="text" value="Email id" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email id';}" required="">	
+											<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" required="">	
+											
+											<input type="submit" value="CREATE ACCOUNT" >
+										</form>
+									</div>
+									<div class="clearfix"></div>								
+								</div>
+								<p>By logging in you agree to our <span>Terms and Conditions</span> and <span>Privacy Policy</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //signup -->
+	 <!-- /w3layouts-agile -->
+		<!-- main content start-->
+		<div class="main-content">
+			<!-- header-starts -->
+			 <?php include("common/header.php"); ?>
+				<!-- //header-ends -->
+							<div id="page-wrapper">
+								<div class="inner-content">
+									<!-- /blog -->
+									
+										<div class="tittle-head">
+											<h3 class="tittle">Our Blogs </h3>
+											<div class="clearfix"> </div>
+										</div>
+										<!-- /music-left -->
+										<div class="music-left">
+											<div class="post-media">
+												  <a href="single.html"><img src="images/33.jpg" class="img-responsive" alt="" /></a>
+												  <div class="blog-text">
+														<a href="single.html"><h3 class="h-t">Lorem Ipsum is simply dummy text</h3></a>
+												      <div class="entry-meta">
+															<h6 class="blg"><i class="fa fa-clock-o"></i> Jan 25, 2016</h6>
+															<div class="icons">
+																<a href="#"><i class="fa fa-user"></i> Admin</a>
+																<a href="#"><i class="fa fa-comments-o"></i> 2</a>
+																<a href="#"><i class="fa fa-thumbs-o-up"></i> 152</a>
+																<a href="#"><i class="fa fa-thumbs-o-down"></i>  26</a>
+															</div>
+																<div class="clearfix"></div>
+															<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non semper, inquam; Quo modo autem philosophus loquitur? Duo Reges: constructio interrete. Dici enim nihil potest verius. Hic ambiguo ludimur. An hoc usque quaque, aliter in vita? Bonum incolumis acies: misera caecitas. Favorite</p>
+													  </div>
+												  </div>
+											</div>
+											
+											<div class="post-media second">
+												  <a href="single.html"><img src="images/11.jpg" class="img-responsive" alt="" /></a>
+												  <div class="blog-text">
+														<a href="single.html"><h3 class="h-t">Lorem Ipsum is simply dummy text</h3></a>
+												      <div class="entry-meta">
+															<h6 class="blg"><i class="fa fa-clock-o"></i> Feb 13, 2016</h6>
+															<div class="icons">
+																<a href="#"><i class="fa fa-user"></i> Admin</a>
+																<a href="#"><i class="fa fa-comments-o"></i> 2</a>
+																<a href="#"><i class="fa fa-thumbs-o-up"></i> 152</a>
+																<a href="#"><i class="fa fa-thumbs-o-down"></i>  26</a>
+															</div>
+																<div class="clearfix"></div>
+															<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non semper, inquam; Quo modo autem philosophus loquitur? Duo Reges: constructio interrete. Dici enim nihil potest verius. Hic ambiguo ludimur. An hoc usque quaque, aliter in vita? Bonum incolumis acies: misera caecitas. Favorite</p>
+													  </div>
+												  </div>
+											</div>
+											
+											<div class="post-media">
+												  <a href="single.html"><img src="images/22.jpg" class="img-responsive" alt="" /></a>
+												  <div class="blog-text">
+														<a href="single.html"><h3 class="h-t">Lorem Ipsum is simply dummy text</h3></a>
+												      <div class="entry-meta">
+															<h6 class="blg"><i class="fa fa-clock-o"></i> Mach 30, 2016</h6>
+															<div class="icons">
+																<a href="#"><i class="fa fa-user"></i> Admin</a>
+																<a href="#"><i class="fa fa-comments-o"></i> 2</a>
+																<a href="#"><i class="fa fa-thumbs-o-up"></i> 152</a>
+																<a href="#"><i class="fa fa-thumbs-o-down"></i>  26</a>
+															</div>
+																<div class="clearfix"></div>
+															<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non semper, inquam; Quo modo autem philosophus loquitur? Duo Reges: constructio interrete. Dici enim nihil potest verius. Hic ambiguo ludimur. An hoc usque quaque, aliter in vita? Bonum incolumis acies: misera caecitas. Favorite</p>
+													  </div>
+												  </div>
+											</div>
+											<!--start-blog-pagenate-->
+												<div class="blog-pagenat">
+													<ul>
+														<li><a class="frist" href="#">Prev</a></li>
+														<li><a href="#">1</a></li>
+														<li><a href="#">2</a></li>
+														<li><a href="#">3</a></li>
+														<li><a href="#">4</a></li>
+														<li><a href="#">5</a></li>
+														<li><a class="last" href="#">Next</a></li>
+														<div class="clearfix"> </div>
+													</ul>
+												</div>
+												<!--//end-blog-pagenate-->
+
+										</div>
+										<!-- //music-left-->
+										<!-- /music-right-->
+										<div class="music-right">
+											<!-- //widget -->
+											  <div class="widget-side">
+												<h4 class="widget-title">Recent Posts</h4>
+													<ul>
+														<li>
+															<a href="single.html">Taylor Swift – Shake It Off</a>
+															<span class="post-date">Feb 13, 2016</span>
+														</li>
+														<li>
+															<a href="single.html">Love Me Like You Do – Ellie Goulding (Fifty Shades of Grey Soundtrack) HQ</a>
+															<span class="post-date">Feb 14, 2016</span>
+														</li>
+														<li>
+															<a href="single.html">Jessie J – Flashlight (from Pitch Perfect 2)</a>
+															<span class="post-date">Feb 16, 2016</span>
+														</li>
+														<li>
+															<a href="single.html">Sol – “Ain’t Gon’ Stop”</a>
+															<span class="post-date">Feb 18, 2016</span>
+														</li>
+														<li>
+															<a href="single.html">Eminem – No Love (Explicit Version) ft. Lil Wayne</a>
+															<span class="post-date">Feb 19, 2016</span>
+														</li>
+													</ul>
+												 </div>
+												 <div class="widget-side second">
+												<h4 class="widget-title">Top Songs</h4>
+													<ul>
+														<li>
+															<div class="song-img">
+															  <a href="single.html"><img src="images/play1.png" class="img-responsive" alt="" /></a>
+															</div>
+															<div class="song-text">
+																<a href="single.html">Ellie-Goulding</a>
+																<span class="post-date">Feb 13, 2016</span>
+															</div>
+															<div class="clearfix"></div>
+														</li>
+														<li>
+															<div class="song-img">
+															  <a href="single.html"><img src="images/play2.png" class="img-responsive" alt="" /></a>
+															</div>
+															<div class="song-text">
+																<a href="single.html">Mark-Ronson-Uptown</a>
+																<span class="post-date">Feb 14, 2016</span>
+															</div>
+															<div class="clearfix"></div>
+														</li>
+														<li>
+															<div class="song-img">
+															  <a href="single.html"><img src="images/play4.png" class="img-responsive" alt="" /></a>
+															</div>
+															<div class="song-text">
+																<a href="single.html">Pharrell-Williams</a>
+																<span class="post-date">Feb 16, 2016</span>
+															</div>
+															<div class="clearfix"></div>
+														</li>
+														<li>
+															<div class="song-img">
+															  <a href="single.html"><img src="images/play5.png" class="img-responsive" alt="" /></a>
+															</div>
+															<div class="song-text">
+																<a href="single.html">Taylor Swift – Shake It Off</a>
+																<span class="post-date">Feb 18, 2016</span>
+															</div>
+															<div class="clearfix"></div>
+														</li>
+														
+													</ul>
+												 </div>
+											  <!-- //widget -->
+										</div>
+										<div class="clearfix"></div>
+									<!-- //blog -->
+								</div>
+							<div class="clearfix"></div>
+						<!--body wrapper end-->
+	 <!-- /w3l-agile -->
+					</div>
+			  <!--body wrapper end-->
+			       <div class="footer two">
+				 <?php include("common/footer.php"); ?>
+				</div>
+			</div>
+		<!-- /wthree-agile -->
+        <!--footer section start-->
+			 <?php include("common/copy-right.php"); ?>
+        <!--footer section end-->
+
+      <!-- main content end-->
+   </section>
+  
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
